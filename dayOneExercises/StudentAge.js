f64288a192ef35205527b025f574c0c985a3d22b
kindyluv <onyeukwuamara@gmail.com>
2021-11-18 16:17:53 +0100

    Day_One_Array

--- dayOneExercises/StudentAge.js
+++ dayOneExercises/StudentAge.js
@@ -5,3 +5,9 @@ console.log(sorted)
 let maxAge = Math.max.apply(Math, ages)
 let minAge = Math.min.apply(Math, ages)
 console.log("maximum age " + maxAge, "minimum age " +minAge);
+
+let median = Math.floor(sorted.length/2);
+if(sorted.length %2 === 0){
+    console.log(sorted[median]);
+}
+console.log(median)
