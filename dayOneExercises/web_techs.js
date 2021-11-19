708df428435664ededc2d5b7ef58d3a00fb13813
kindyluv <onyeukwuamara@gmail.com>
2021-11-19 04:38:41 +0100

    Day_One_

---
+++ dayOneExercises/web_techs.js
@@ -0,0 +1,7 @@
+const web_techArray = [5];
+
+web_techArray[0] = 'HackerRank';
+web_techArray[1] = 'LeetCode';
+web_techArray[2] = 'WsSchool';
+web_techArray[3] = 'BootStrap';
+web_techArray[4] = 'FreeCodeCamp';
