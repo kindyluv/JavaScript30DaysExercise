b835c1b6fd3eff1a7c91caba31c00f8403512288
kindyluv <onyeukwuamara@gmail.com>
2021-11-19 04:38:50 +0100

    Day_One_

---
+++ dayOneExercises/Countries.js
@@ -0,0 +1,12 @@
+const countriesArray = [10];
+
+countriesArray[0] = "Africa";
+countriesArray[1] = "Afghanistan";
+countriesArray[2] = "Iran";
+countriesArray[3] = "India";
+countriesArray[4] = "Canada";
+countriesArray[5] = "Mexico";
+countriesArray[6] = "Georgie";
+countriesArray[7] = "Germany";
+countriesArray[8] = "Poland";
+countriesArray[9] = "Sweden";
